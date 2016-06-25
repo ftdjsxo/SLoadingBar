8705d58133421d867997ca68621ab78e8266cdf6
Francesco Thiery <xiomxiom1@gmail.com>
2016-06-25 14:16:07 +0200

    Initial Commit

---
+++ SLoadingBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SLoadingBar
+//
+//  Created by Francesco Thiery on 25/06/16.
+//  Copyright © 2016 Francesco Thiery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
